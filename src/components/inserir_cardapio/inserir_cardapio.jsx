a8ab4b951fc21b03df593bd6b4150b89556413b7
NodjaLima <nodjamplima@gmail.com>
2022-09-01 17:31:46 -0300

    criado componente inserir_cardapio

---
+++ src/components/inserir_cardapio/inserir_cardapio.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useState } from 'react';
+
+const InserirCardapio = () => {
+
+  const [categoria, setCategoria] = useState();
+  const [sabor, setSabor] = useState();
+  const [ingredientes, setIngredientes] = useState();
+  const [tamanho, setTamanho] = useState();
+  const [preco, setPreco] = useState();
+
+  const post = async () => {
+    await axios.post('https://restaurante-api-m4.herokuapp.com/cardapio/novo', {
+      categoria_cardapio : categoria,
+      sabor_cardapio : sabor,
+      ingredientes_cardapio : ingredientes,
+      tamanho_cardapio : tamanho,
+      valor_cardapio : preco
+  })
+
+  
+
+  console.log(categoria)
+  }
+
+  return ( 
+    <div className="post-cardapio">
+      <input type='text' placeholder="Categoria" onBlur={(e) => {setCategoria(e.target.value)}}/>
+      <input type='text' placeholder="Sabor" onBlur={(e) => {setSabor(e.target.value)}}/>
+      <input type='text' placeholder="Ingredientes" onBlur={(e) => {setIngredientes(e.target.value)}}/>
+      <input type='text' placeholder="Tamanho" onBlur={(e) => {setTamanho(e.target.value)}}/>
+      <input type='text' placeholder="Preço" onBlur={(e) => {setPreco(e.target.value)}}/>
+      <button onClick={post}>Cadastrar Item</button>
+    </div>
+   );
+}
+ 
+export default InserirCardapio;
--- src/pages/cardapio/cardapio.jsx
+++ src/pages/cardapio/cardapio.jsx
@@ -1,14 +1,50 @@
-import { Link } from "react-router-dom";
+import { useState } from 'react'
+import InserirCardapio from '../../components/inserir_cardapio/inserir_cardapio';
+import ListaCardapio from "../../components/lista_cardapio/lista_cardapio";
 
 const Cardapio = () => {
+  const [mostraCardapio, setMostraCardapio] = useState(false)
+  const [editarCardapio, setEditarCardapio] = useState(false)
+  const [inserirCardapio, setInserirCardapio] = useState(false)
+  const [deletarCardapio, setDeletarCardapio] = useState(false)
+
+
   return ( 
-    <spam>
+    <div className="cardapio-container">
+      
       <h1>GERENCIE O SEU CARDÁPIO</h1>
-      <Link to='/cardapio_completo'><button>Vizualizar</button></Link>
-      <Link to='/cardapio_inserir'><button>Inserir</button></Link>
-      <Link to='/cardapio_editar'><button>Editar</button></Link>
-      <Link to='/cardapio_deletar'><button>Deletar</button></Link>
-    </spam>
+      <button onClick={()=>{
+        setMostraCardapio(true)
+        setEditarCardapio(false)
+        setInserirCardapio(false)
+        setDeletarCardapio(false)}
+        }>Vizualizar</button>
+      <button onClick={()=>{
+        setMostraCardapio(false)
+        setEditarCardapio(false)
+        setInserirCardapio(true)
+        setDeletarCardapio(false)}
+        }>Inserir</button>
+      <button onClick={()=>{
+        setMostraCardapio(false)
+        setEditarCardapio(true)
+        setInserirCardapio(false)
+        setDeletarCardapio(false)}
+        }>Editar</button>
+      <button onClick={()=>{
+        setMostraCardapio(false)
+        setEditarCardapio(false)
+        setInserirCardapio(false)
+        setDeletarCardapio(true)}
+        }>Deletar</button>
+      
+
+      {mostraCardapio && <ListaCardapio />}
+      {inserirCardapio && <InserirCardapio />}
+
+
+    </div>
+    
    );
 }
  
