167f44bc4e80f5962a0c5bf26b35bcf1563022a5
NodjaLima <nodjamplima@gmail.com>
2022-09-01 17:29:28 -0300

    criado componente lista_cardapio

---
+++ src/components/lista_cardapio/lista_cardapio.jsx
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import ItemCardapio from '../item_cardapio/item_cardapio'
+
+const ListaCardapio = () => {
+
+  const [cardapio, setCardapio] = useState([])
+
+  useEffect(() => {axios.get('https://restaurante-api-m4.herokuapp.com/cardapio/tudo')
+  .then((response) => setCardapio(response.data))
+  .catch((e) => e.message)}, [])
+
+  console.log(cardapio)
+  
+  return ( 
+    <div>
+      {
+        cardapio.map((item) => {
+          return (
+            <ItemCardapio nome={item.sabor_cardapio} ingredientes={item.ingredientes_cardapio} tamanho={item.tamanho_cardapio} valor={item.valor_cardapio}/>
+          )
+        })
+      }
+
+
+    </div>
+         
+   );
+}
+ 
+export default ListaCardapio;
