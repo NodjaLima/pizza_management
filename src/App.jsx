9af4606e5f9debf21a6d857dc7f896558904b5ce
larrissalira <larrissalira@outlook.com>
2022-09-01 00:00:07 -0300

    criação da pasta e arquivo router.jsx, para melhor organização das rotas, importando o componente 'Header' e a page 'fornecedores'

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Router from './routes/router'
 import './App.css'
-import Menu from './components/menu/Menu'
-import Home from './pages/home/Home'
 
 function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
-          <Menu />
-          <Routes>
-            //para criar rotas usar letras minuscular e separar palavras com _
-            <Route path='/' exact element={<Home />} />
-            <Route path='/cardapio' exact element={<h1>Esse será o cardapio</h1>} />
-            <Route path='/clientes' exact element={<h1>Essa será a pagina de clientes</h1>} />
-            <Route path='/colaboradores' exact element={<h1>Essa será a pagina de colaboradores</h1>} />
-            <Route path='/fornecedores' exact element={<h1>Essa será a essa será a pagina de fornecedores</h1>} />
-          </Routes>
-        </BrowserRouter>
+      <Router /> 
     </div>
   )
 }
---
+++ src/routes/router.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '../pages/home/Home.jsx'
+import Header from '../components/header/Header.jsx'
+import Fornecedores from '../pages/fornecedores/fornecedores.jsx'
+
+export default function Router() {
+
+    return (
+        <BrowserRouter>
+        <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/cardapio' exact element={<cardapio />} />
+            <Route path='/clientes' exact element={<clientes/>} />
+            <Route path='/colaboradores' exact element={<colaboradores/>} />
+            <Route path='/fornecedores' exact element={<Fornecedores />} /> 
+          </Routes>
+        </BrowserRouter>
+    )
+}
