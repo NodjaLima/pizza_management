5c4126315c1f60be0e86dc0d0a697fccbf3b570c
Renato Negrelly <renatonegrelly@gmail.com>
2022-09-08 14:26:13 -0300

    implementado método delete para deleção de clientes.

--- src/pages/clientes/delete-clientes/clientedeletar.jsx
+++ src/pages/clientes/delete-clientes/clientedeletar.jsx
@@ -7,8 +7,7 @@ import '../../clientes/clientes.css'
 
 const Deletaclientes = () => {
 
-
-
+    //requisição de dados do cliente com atribuição à variavel options.
     const [options, setOptions] = useState([]);
 
     useEffect(() =>{
@@ -20,14 +19,31 @@ const Deletaclientes = () => {
 
     }, [])
 
+    //bloco para preenchimento dos campos <p> após seleção do id do cliente
+    const [infoCliente, setInfoCliente] = useState ([]);
+
+    //bloco apara deletar o cliente referente ao id selecionado
+
+    function onclick(event){
+        event.preventDefault()
+        
+        let idc = 4
+        const deletarcliente = confirm(`Deseja excluir o cliente ID: ${idc}?`)
+        
+        if(deletarcliente == true){
+
+            axios.delete(`https://restaurante-api-m4.herokuapp.com/clientes/${idc}`)
+            .then(response => {
+            
+                alert('Cliente deletado.')
+        })}        
+        
+    }
+
     if(options.length===0){
         return null
     }
-
-    // const ids = Object.values(options[0])
-    // // console.log(ids)
-    // console.log(options[0].id_cliente)
-        
+       
     return (
 
         <div>
@@ -44,36 +60,55 @@ const Deletaclientes = () => {
                         <option>Selecione</option>
 
                         {options.map((id, index) =>{
-                             return(
-                                <option key={index} value={id.id_cliente}>
-                                    {id.id_cliente}
-                                </option>
-                                  // console.log(id.id_cliente)
-                             )
-                              
+                                return(
+                                    <option key={index} value={id.id_cliente}>
+                                        {id.id_cliente}
+                                    </option>
+                                    // console.log(id.id_cliente)
+                                )                              
                             })}                    
                     </select>
                 </label>
 
             </p><br></br><br></br><br></br>
 
-
-            <p>Nome: </p>
+            <div className="div-retorno-dados">
+            <div><p> Nome: </p></div>
+            <div><p> Renato Tadeu Negrelly junior </p></div>
+            </div>                
+            
+            <hr></hr><br></br><br></br>
+            
+            <div className="div-retorno-dados">
+            <div><p> CPF: </p></div>
+            <div><p> 13815800765 </p></div>
+            </div>                
+            
             <hr></hr><br></br><br></br>
 
-            <p>CPF: </p>
+            <div className="div-retorno-dados">
+            <div><p> Endereço: </p></div>
+            <div><p> Rua Nossa Senhora das Dores, 591 - Ilha do Governador, RJ </p></div>
+            </div>
+            
+            <hr></hr><br></br><br></br>
+            
+            <div className="div-retorno-dados">
+            <div><p> Email: </p></div>
+            <div><p> renato@gmail.com </p></div>
+            </div>
+            
+            
             <hr></hr><br></br><br></br>
 
-            <p>Endereço: </p>
+            <div className="div-retorno-dados">
+            <div><p> Telefone: </p></div>
+            <div><p> 21988888888 </p></div>
+            </div>                
+            
             <hr></hr><br></br><br></br>
 
-            <p>Email: </p>
-            <hr></hr><br></br><br></br>
-
-            <p>Telefone: </p>
-            <hr></hr><br></br><br></br>
-
-            <button className="btn-cliente">Deletar</button>
+            <button className="btn-cliente" onClick={onclick}>Deletar</button>
 
 
         </div>
