8c1462b045d7457f8d26387075a7d42a8ed2c328
larrissalira <larrissalira@outlook.com>
2022-09-08 16:11:58 -0300

    insere a function fornecedores para exibir e inserir novo cadastro no forms da entidade Fornecedores, correção do post e get da requisição

--- src/pages/fornecedores/Fornecedores.jsx
+++ src/pages/fornecedores/Fornecedores.jsx
@@ -1,7 +1,23 @@
-import { useEffect } from "react"
-import api from "./api"
+import { useState } from "react";
+import FormsFornecedores from "../../components/fornecedores/box_forms_fornecedores/forms_fornecedores"
+import BoxMenu from "../../components/fornecedores/box_menu_fornecedores/boxMenu"
+import ListaFornecedores from "../../components/fornecedores/box_lista_fornecedores/lista_fornecedores";
+import './fornecedores.css'
 
 export default function Fornecedores() {
-    return (<h1>foi</h1>)
+  const [exibir, setExibir] = useState(false);
+  const [inserir, setInserir] = useState(false);
 
-}
+  return (
+    <div className = "box">
+      
+      <BoxMenu onClickExibir = {e => {e.preventDefault(); setExibir(true); setInserir(false)}} 
+       onClickInserir = {e => {e.preventDefault(); setInserir(true); setExibir(false)
+      }}/>
+      
+      {exibir && <ListaFornecedores/>}
+      {inserir && <FormsFornecedores/>}
+    </div>
+
+  );
+};
--- src/pages/fornecedores/fornecedores_api.js
+++ src/pages/fornecedores/fornecedores_api.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-export const postFornecedores = (fornecedores) => {
-    return axios.post('https://restaurante-api-m4.herokuapp.com/fornecedores', fornecedores).then((response)=> response.data)
+export const postFornecedores = async (fornecedores) => {
+    const response = await axios.post('https://restaurante-api-m4.herokuapp.com/fornecedores', fornecedores);
+  return response.data;
 }
 
-export const getFornecedores = () => {
-  return axios.get('https://restaurante-api-m4.herokuapp.com/fornecedores').then((response)=> response.data)
+export const getFornecedores = async () => {
+  const response = await axios.get('https://restaurante-api-m4.herokuapp.com/fornecedores');
+  return response.data;
 }
 
 export const deleteFornecedores = (id) => {
