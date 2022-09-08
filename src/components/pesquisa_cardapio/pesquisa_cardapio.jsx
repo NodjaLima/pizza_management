c726521951fc89bcfebb5761cfed392977dd550c
NodjaLima <nodjamplima@gmail.com>
2022-09-08 17:05:55 -0300

    criado pagina de pesquisa na entidade cardapio

---
+++ src/components/pesquisa_cardapio/pesquisa_cardapio.jsx
@@ -0,0 +1,59 @@
+import { PesquisaCardapioPorCategoria } from '../../services/requisicoes_cardapio'
+import { useState } from 'react'
+import './pesquisa_cardapio.css'
+
+const PesquisaCardapio = () => {
+
+  const [pesquisa, setPesquisa] = useState()
+  const [select, setSelect] = useState()
+  const [inputValue, setInputValue] = useState()
+  const list = [
+    {id: 0, name: 'Selecione'},
+    {id: 1, name: 'categoria'},
+    {id: 2, name: 'sabor'},
+    
+  ];
+  const [mostraPesquisa, setMostraPesquisa] = useState(false)
+
+  const Pesquisar = (parametro, valor) => {
+    PesquisaCardapioPorCategoria(parametro, valor)
+    .then((response) => {
+      setPesquisa(response.data)
+      setMostraPesquisa(true)})
+    .catch((e) => { alert('não foi encontrado nenhum item com os parametros pesquisados')})
+  }
+
+  return ( 
+    <div className='pesquisa-container'>
+      <div className='pesquisar'>
+        <select onChange={(e)=> {setSelect(e.target.value)}} value={select}>
+          {list.map((item, index) => (
+          <option value={item.name}>{item.name}</option>
+        ))}    
+        </select>
+          <input type="text" onBlur={(e)=>{setInputValue(e.target.value)}}/>
+          <button onClick={()=> {
+            Pesquisar(select, inputValue)
+          }}>Pesquisar</button>
+      </div>
+
+      {mostraPesquisa && pesquisa.map((item) => { 
+        return (
+          <div>
+            <div className='item-cardapio-container'>
+              <h4>{item.sabor_cardapio}</h4>
+              <br />
+              <p>Ingredientes: {item.ingredientes_cardapio}</p>
+              <p>Tamanho: {item.tamanho_cardapio}</p>
+              <p>Valor: {item.valor_cardapio}</p>
+            </div>
+          </div>
+        )
+
+      })}
+
+    </div>
+   );
+}
+ 
+export default PesquisaCardapio;
--- src/pages/cardapio/cardapio.jsx
+++ src/pages/cardapio/cardapio.jsx
@@ -4,8 +4,10 @@ import ListaCardapio from "../../components/lista_cardapio/lista_cardapio";
 import CardapioInicial from  '../../components/cardapio_inicial/cardapio_inicial';
 
 import './cardapio.css'
+import PesquisaCardapio from '../../components/pesquisa_cardapio/pesquisa_cardapio';
 
 const Cardapio = () => {
+  const [pesquisaCardapio, setPesquisaCardapio] = useState(false)
   const [mostraCardapio, setMostraCardapio] = useState(false)
   const [inserirCardapio, setInserirCardapio] = useState(false)
   const [inicio, setInicio] = useState(true)
@@ -20,19 +22,28 @@ const Cardapio = () => {
       <button onClick={()=>{
         setMostraCardapio(true)
         setInserirCardapio(false)
+        setPesquisaCardapio(false)
         setInicio(false)}
         }>Vizualizar</button>
       <button onClick={()=>{
         setMostraCardapio(false)
         setInserirCardapio(true)
+        setPesquisaCardapio(false)
         setInicio(false)}
         }>Inserir</button>
+      <button onClick={()=>{
+        setMostraCardapio(false)
+        setInserirCardapio(false)
+        setPesquisaCardapio(true)
+        setInicio(false)}
+        }>Pesquisar</button>
       
       </div>
       
       {inicio && <CardapioInicial />}
       {mostraCardapio && <ListaCardapio />}
       {inserirCardapio && <InserirCardapio />}
+      {pesquisaCardapio && <PesquisaCardapio />}
 
     </div>
     
