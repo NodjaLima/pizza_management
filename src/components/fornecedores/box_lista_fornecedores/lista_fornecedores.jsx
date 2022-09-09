14dc6e50211d8996728f521d1c6b7f18feb62038
larrissalira <larrissalira@outlook.com>
2022-09-08 23:22:34 -0300

    adicionar img editar e excluir e estilização da lista_fornecedores

--- src/components/fornecedores/box_lista_fornecedores/lista_fornecedores.jsx
+++ src/components/fornecedores/box_lista_fornecedores/lista_fornecedores.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { deleteFornecedores } from "../../../pages/fornecedores/fornecedores_api";
 import "./lista_fornecedores.css";
+import excluir from "../../../assets/img/excluir_fornecedor.png"
+import editar from "../../../assets/img/editar_fornecedor.png"
 
 export default function ListaFornecedores (){
   const [info, setInfo] = useState([]);
@@ -33,6 +36,14 @@ export default function ListaFornecedores (){
               <td id="td-ramo">{item.ramo_fornecedores}</td>
               <td id="td-email">{item.email_fornecedores}</td>
               <td id="td-telefone">{item.telefone_fornecedores}</td>
+           
+               <img src={editar}></img>
+              
+                <a id='td-a2' await onClick={async () => {
+                  const pergunta = confirm(`Excluir fornecedor ${item.nome_fornecedores}?`)
+                  pergunta ? await deleteFornecedores(item.ramo_fornecedores).then(alert('Fornecedor excluído!')) : alert('Cancelado!')
+                  }}><img src={excluir}></img>
+                </a>
             </tr>
             </>
           );
