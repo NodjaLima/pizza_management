515988b5625d803cd32c0e8c905ad92665102caa
devhelderlrs <helderl4004@gmail.com>
2022-09-09 01:44:11 -0300

    resolvido conflito router.jsx

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,8 @@
 import { BrowserRouter } from 'react-router-dom'
 import Header from '../components/header/Header.jsx'
 import { LoginProvider } from '../context/context_login.jsx'
-<<<<<<< HEAD
 import { ColaboradorProvider } from '../context/context_colaborador.jsx'
-=======
 import { AppRoutes } from './routes.jsx'
->>>>>>> aca0cb4943a5a0560be4de898a087b0894063f9d
 
 
 export default function Router() {
