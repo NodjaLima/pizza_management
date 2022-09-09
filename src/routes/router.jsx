6b87b677d7f088842b9b5a3ba071209ed3475cb5
NodjaLima <nodjamplima@gmail.com>
2022-09-08 23:43:57 -0300

    criação de arquivo para complartilhar contexto de login

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -5,23 +5,19 @@ import Fornecedores from '../pages/fornecedores/fornecedores.jsx'
 import Colaboradores from '../pages/colaboradores/Colaboradores.jsx'
 import Cardapio from '../pages/cardapio/cardapio.jsx'
 import Login from '../pages/login/login.jsx'
-import { LoginProvider } from '../context/context_login.jsx'
+import { LoginContext, LoginProvider } from '../context/context_login.jsx'
+import { useContext } from 'react'
+import { AppRoutes } from './routes.jsx'
 
 
 export default function Router() {
+
 
     return (
         <LoginProvider>
           <BrowserRouter>
             <Header />
-              <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/cardapio' exact element={<Cardapio />} />
-                <Route path='/clientes' exact element={<clientes/>} />
-                <Route path='/colaboradores' exact element={<Colaboradores/>} />
-                <Route path='/fornecedores' exact element={<Fornecedores />} />
-                <Route path='/login' exact element={<Login />} />
-              </Routes>
+              <AppRoutes />
           </BrowserRouter>
         </LoginProvider>
     
---
+++ src/routes/routes.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom"
+import { useContext } from "react"
+import { LoginContext } from '../context/context_login.jsx'
+import Fornecedores from '../pages/fornecedores/fornecedores.jsx'
+import Colaboradores from '../pages/colaboradores/Colaboradores.jsx'
+import Cardapio from '../pages/cardapio/cardapio.jsx'
+import Login from '../pages/login/login.jsx'
+import Home from '../pages/home/Home.jsx'
+import { GetColaboradores } from '../services/requisicoes_colaboradores'
+
+export const AppRoutes = () => {
+
+  const { usuario } = useContext(LoginContext)
+ 
+  return (
+
+    <Routes>
+    <Route path='/' element={<Home />} />
+    {usuario.length == 1 && <Route path='/cardapio' exact element={<Cardapio />} />}
+    {usuario.length == 1 && <Route path='/clientes' exact element={<clientes/>} />}
+    {usuario.length == 1 && <Route path='/colaboradores' exact element={<Colaboradores/>} />}
+    {usuario.length == 1 && <Route path='/fornecedores' exact element={<Fornecedores />} />}
+    <Route path='/login' exact element={<Login />} />
+    </Routes>
+
+  )
+}
