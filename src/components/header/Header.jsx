7728df997036bc6be8bf2f245dd5e6a2cb7d8de5
NodjaLima <nodjamplima@gmail.com>
2022-09-09 14:25:42 -0300

    insere link Home na navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,13 +12,18 @@ export default function Header() {
             <img src={logo} onClick={() => navegate("/")}></img>
         </div>
         <div className="links-menu">  
+            <div className="link" onClick={() => navegate("/boas-vindas")}>
+               Home
+            </div>
+            
             <div className="link" onClick={() => navegate("/sobre")}>
                Sobre
             </div>
-            
+
             <div className="link"onClick={() => navegate("/")}>
                Login
             </div>
+
         </div>
       </div>
     )
