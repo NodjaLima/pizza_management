79553a5110ca83d3a6f6c79fb3a42082b215443e
NodjaLima <nodjamplima@gmail.com>
2022-09-09 00:22:57 -0300

    aplica estilização nos botões da entidade cardapio

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,9 +4,9 @@ import { LoginContext } from '../context/context_login.jsx'
 import Fornecedores from '../pages/fornecedores/fornecedores.jsx'
 import Colaboradores from '../pages/colaboradores/Colaboradores.jsx'
 import Cardapio from '../pages/cardapio/cardapio.jsx'
+import Clientes from '../pages/clientes/clientes.jsx'
 import Login from '../pages/login/login.jsx'
 import Home from '../pages/home/Home.jsx'
-import { GetColaboradores } from '../services/requisicoes_colaboradores'
 
 export const AppRoutes = () => {
 
@@ -17,7 +17,7 @@ export const AppRoutes = () => {
     <Routes>
     <Route path='/' element={<Home />} />
     {usuario.length == 1 && <Route path='/cardapio' exact element={<Cardapio />} />}
-    {usuario.length == 1 && <Route path='/clientes' exact element={<clientes/>} />}
+    {usuario.length == 1 && <Route path='/cliente' exact element={<Clientes/>} />}
     {usuario.length == 1 && <Route path='/colaboradores' exact element={<Colaboradores/>} />}
     {usuario.length == 1 && <Route path='/fornecedores' exact element={<Fornecedores />} />}
     <Route path='/login' exact element={<Login />} />
